5cf06cdc6e470e7ab547dc66ce7acc60c64c4aa4
Amy Westcott <amy.westcott@yahoo.com>
2019-08-08 19:56:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,21 @@ class App < Sinatra::Base
   end 
   
   get '/:operation/:number1/:number2' do 
-    
+    operation, num1, num2 = params.values
+    num1 = num1.to_i 
+    num2 = num2.to_i
+
+    case operation
+    when "add"
+      "#{num1 + num2}"
+    when "subtract"
+      "#{num1 - num2}"
+    when "multiply"
+      "#{num1 * num2}"
+    when "divide"
+      "#{num1 / num2}"  
+    end
+  end
   end 
 
 end
