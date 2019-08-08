e20ab308fd1fb6619a7c91c75234d3ece7c645e5
Amy Westcott <amy.westcott@yahoo.com>
2019-08-08 19:54:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,9 +11,7 @@ class App < Sinatra::Base
   
   get "/say/:number/:phrase" do 
    "#{Array.new(params[:number].to_i, params[:phrase]).join(", ")}"
-    
-     
-  end 
+    end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
   end 
