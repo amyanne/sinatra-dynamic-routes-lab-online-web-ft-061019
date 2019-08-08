516eb4530a0898b0dde922573b45a90576b1d9ab
Amy Westcott <amy.westcott@yahoo.com>
2019-08-08 19:22:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get "/reversename/:name" do 
+     "#{params[:name].reverse}"
   end 
+  
+  get "/square/:number" do 
+    "#{params[:number]"
 
 end
